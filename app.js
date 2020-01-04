87b4bc55161a812fa1323f7b87bba4206f7ae3f0
Shreyans Jain <shreyansjain012@gmail.com>
2020-01-04 11:52:21 +0530

    Connected public folder for static files and templates

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 //jshint esversion:6
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello")
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//tell express to serve up this public folder as a static resource.
+app.use(express.static('public'));
+
+//listens to all GET requests
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
-
-app.listen(3000, function(){
-  console.log("Server started on port 3000");
+app.listen(3000, function() {
+  console.log('Server started on port 3000');
 });
