ab5e47fd96f6b491fdb7aa55bf685e3210bd4b21
Shreyans Jain <shreyansjain012@gmail.com>
2020-01-31 17:14:19 +0530

    Added comments

--- public/js/makeChart.js
+++ public/js/makeChart.js
@@ -1,48 +1,76 @@
-let myChart = document.getElementById('myChart').getContext('2d');
+// Kinetic data for saponification of ethyl acetate
+const A = 3.23 * Math.pow(10,7); // Frequency factor (L/mol.s)
+const Ea = 48325.2; // Activation energy (Joule/mol)
+const R = 8.314 // universal gas constant in SI units
+
 const pi = Math.PI;
 
+let myChart = document.getElementById('myChart').getContext('2d');
 
+/*
+ * Uses Arrhenius equation to calculate the rate constant for a reaction 
+ *
+ * @param   (Number)    A    frequency factor
+ * @param   (Number)    Ea   activation energy for the reaction 
+ * @param   (Number)    R    universal gas constant
+ * @param   (Number)    T    temperature during reaction     
+ *    
+ * @return  (Number)    rate constant of a reaction
+ */
 function rateConstant (A,Ea,R,T) {
-    // Arrhenius equation
-    T = T + 273;
+    T = T + 273; // temperature conversion
     return A*Math.exp(-Ea/(R*T)); 
 }
 
+/*
+ * PFR volume is given by pi*(r^2)*l
+ *
+ * @param   (Number)    d    diameter of PFR
+ * @param   (Number)    l    length of PFR
+ *    
+ * @return  (Number)    Volume of PFR 
+ */
 function pfrVol (d,l) {
-    d = d/100;
+    d = d/100;  // cm to m conversion
     return pi * Math.pow(d,2) * l / 4 * 1000; 
 }
 
-function getCa0 (Fa, Fb, Na, Nb){
-    
+
+/*
+ * @param   (Number)    Fa    molar flow rate of A
+ * @param   (Number)    Fb    molar flow rate of B
+ * @param   (Number)    Na    normality of A
+ * @param   (Number)    Nb    normality of B
+ *    
+ * @return  (Number)    initial concentration of A 
+ */
+function getCa0 (Fa, Fb, Na, Nb){  
     return Fa * Na / (Fa + Fb);
 }
 
-function getCb0 (Fa, Fb, Na, Nb){
-    
+/*
+ * @param   (Number)    Fa    molar flow rate of A
+ * @param   (Number)    Fb    molar flow rate of B
+ * @param   (Number)    Na    normality of A
+ * @param   (Number)    Nb    normality of B
+ *    
+ * @return  (Number)    initial concentration of B
+ */
+function getCb0 (Fa, Fb, Na, Nb){    
     return Fb * Nb / (Fa + Fb);
 }
 
-function cstrEqn (k, Ca0, Cb0, tau2, Xa1) {
+/*
+ * @param   (Number)    k       rate constant
+ * @param   (Number)    Ca0     initial conc. of A
+ * @param   (Number)    Cb0     initial conc. of B
+ * @param   (Number)    tau1    time constant of PFR
+ * @param   (Number)    Xa1     conversion from previous reactor
+ *    
+ * @return  (Number)    conversion from PFR
+ */
+function pfr (k, Ca0, Cb0, tau1, Xa1) {
     let M = Cb0/Ca0;
-    let a = 1;
-    let b = (M+1)+1/(k*tau2*Ca0);
-    let c = M + Xa1/(k*tau2*Ca0);
-    
-    let res1 = (b + Math.sqrt(b*b - 4*a*c))/(2*a);
-    let res2 = (b - Math.sqrt(b*b - 4*a*c))/(2*a);
-
-    if(res1 > Xa1 && res1 <= 1){
-        return res1;
-    }
-    else {
-        return res2;
-    }
-}
-
-function pfrEqn (k, Ca0, Cb0, tau1, Xa1) {
-    let M = Cb0/Ca0;
-    console.log('k = ' + k + ' Ca0 = ' + Ca0 + ' Cb0 = ' + Cb0 + ' tau1 = ' + tau1 + ' M = ' + M);
     if(M < 1){
         let a = (M - Xa1) * Math.exp(k*tau1*Ca0*(M-1)) - (1 - Xa1) * M;  
         let b = (M - Xa1) * Math.exp(k*tau1*Ca0*(M-1)) - (1 - Xa1);
@@ -59,40 +87,46 @@ function pfrEqn (k, Ca0, Cb0, tau1, Xa1) {
     }
 }
 
-let reactorList = [];
+/*
+ * @param   (Number)    k       rate constant
+ * @param   (Number)    Ca0     initial conc. of A
+ * @param   (Number)    Cb0     initial conc. of B
+ * @param   (Number)    tau2    time constant of CSTR
+ * @param   (Number)    Xa1     conversion from previous reactor
+ *    
+ * @return  (Number)    conversion from CSTR 
+ */
+function cstr (k, Ca0, Cb0, tau2, Xa1) {
+    let M = Cb0/Ca0;
 
-function addpfr() {
-    reactorList.push('pfr');
+    // solving quadratic equation ax^2 + bx + c = 0
+    let a = 1;
+    let b = (M+1)+1/(k*tau2*Ca0);
+    let c = M + Xa1/(k*tau2*Ca0);
+    let res1 = (b + Math.sqrt(b*b - 4*a*c))/(2*a);
+    let res2 = (b - Math.sqrt(b*b - 4*a*c))/(2*a);
+
+    if(res1 > Xa1 && res1 <= 1){
+        return res1;
+    }
+    else {
+        return res2;
+    }
 }
 
-function addcstr(){
-    reactors.push('cstr');
-}
-
-
-
-function myFunction() {
-    // Kinetic data for saponification of ethyl acetate
-    const A = 3.23 * Math.pow(10,7); // Frequency factor (L/mol.s)
-    const Ea = 48325.2; // Activation energy (Joule/mol)
-    const R = 8.314 // SI units
-    
+function myFunction() {   
     // Input data
-    let Fa = Number(document.getElementById("fa").value);
-    let Fb = Number(document.getElementById("fb").value);
-    let Na = Number(document.getElementById("na").value);
-    let Nb = Number(document.getElementById("nb").value);
-    let d = Number(document.getElementById("pfrDiameter").value);
-    let l = Number(document.getElementById("pfrLength").value);
-    let temperature = Number(document.getElementById('temperature').value);
-    let minTemp = Number(document.getElementById('temperature').min);
-    let maxTemp = Number(document.getElementById('temperature').max);
-    let v2 = Number(document.getElementById('cstrVolume').value);
+    const Fa = Number(document.getElementById("fa").value)/(60*60); // converting LPH in LPS
+    const Fb = Number(document.getElementById("fb").value)/(60*60); // converting LPH in LPS
+    const Na = Number(document.getElementById("na").value);
+    const Nb = Number(document.getElementById("nb").value);
+    const d = Number(document.getElementById("pfrDiameter").value);
+    const l = Number(document.getElementById("pfrLength").value);
+    const temperature = Number(document.getElementById('temperature').value);
+    const minTemp = Number(document.getElementById('temperature').min);
+    const maxTemp = Number(document.getElementById('temperature').max);
+    const v2 = Number(document.getElementById('cstrVolume').value);
 
-    
-    // converting LPH in LPS
-    Fa = Fa/(60*60);
-    Fb = Fb/(60*60);
 
     // Creating dataset for chart
     const tempData = [], XaData = [];
@@ -118,18 +152,18 @@ function myFunction() {
 
         switch (myReactor) {
             case 'pfr':
-                Xa1 = pfrEqn(k, Ca0, Cb0, tau1, Xa1);
+                Xa1 = pfr(k, Ca0, Cb0, tau1, Xa1);
                 break;
             case 'cstr':
-                Xa1 = cstrEqn (k, Ca0, Cb0, tau2, Xa1);
+                Xa1 = cstr (k, Ca0, Cb0, tau2, Xa1);
                 break;
             case 'cstr-pfr':
-                Xa1 = cstrEqn (k, Ca0, Cb0, tau2, Xa1);
-                Xa1 = pfrEqn(k, Ca0, Cb0, tau1, Xa1);
+                Xa1 = cstr (k, Ca0, Cb0, tau2, Xa1);
+                Xa1 = pfr(k, Ca0, Cb0, tau1, Xa1);
                 break;
             case 'pfr-cstr':
-                Xa1 = pfrEqn(k, Ca0, Cb0, tau1, Xa1);
-                Xa1 = cstrEqn (k, Ca0, Cb0, tau2, Xa1);
+                Xa1 = pfr(k, Ca0, Cb0, tau1, Xa1);
+                Xa1 = cstr (k, Ca0, Cb0, tau2, Xa1);
                 break;
         }
         console.log('Xa2: ' + Xa1);
